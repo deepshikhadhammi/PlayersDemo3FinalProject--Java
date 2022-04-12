7dc263bea6ac65bd412185f860cbda43124b20b1
Namit Aneja <namit.aneja@ucalgary.ca>
2022-04-12 10:46:10 -0600

    Creating the player and reader file and pushing it to git

--- src/main/java/mvh/app/Player.java
+++ src/main/java/mvh/app/Player.java
@@ -1,3 +1,7 @@
+//DEMO3
+//NAME: DEEPSHIKHA DHAMMI, NAMIT ANEJA
+//DATE: APRIL 12, 2022
+// TUTORIAL: T03, T08
 package mvh.app;
 
 import java.util.ArrayList;
@@ -5,29 +9,51 @@ import java.util.HashMap;
 import java.util.Objects;
 
 public class Player {
-    private static String player_name;
-    private static int player_age;
-    private static int player_weight;
+    private static String player_name;      //instance variable
+    private static int player_age;          //instance variable
+    private static int player_weight;       //instance variable
 
-
+    /**
+     *
+     * @param name to assign name to a player
+     * @param age to assign age to a player
+     * @param weight to assign weight to a player
+     */
     Player(String name, int age, int weight) {
         this.player_name = name;
         this.player_age = age;
         this.player_weight = weight;
     }
 
+    /**
+     *
+     * @return player's weight
+     */
     public static int getPlayerweight() {
         return player_weight;
     }
 
+    /**
+     *
+     * @return player's name
+     */
     public static String getPlayername() {
         return player_name;
     }
+
+    /**
+     *
+     * @return player's age
+     */
 
     public static int getPlayerage() {
         return player_age;
     }
 
+    /**
+     *
+     * @return player's details such as name, age and weight
+     */
     @Override
     public String toString() {
         return "                Player Details       "+"\n"+"Name: " + player_name +"\n"+
--- src/main/java/mvh/app/Reader.java
+++ src/main/java/mvh/app/Reader.java
@@ -1,3 +1,7 @@
+//DEMO3
+//NAME: DEEPSHIKHA DHAMMI, NAMIT ANEJA
+//DATE: APRIL 12, 2022
+// TUTORIAL: T03, T08
 package mvh.app;
 
 import java.io.BufferedReader;
@@ -7,45 +11,55 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Reader {
-    public static ArrayList<Player> player_list=new ArrayList<>();
+    public static ArrayList<Player> player_list=new ArrayList<>();  //initialising arraylist
+    //magic numbers initialised
     static int number_zero=0;
     static int number_one=1;
     static int number_two=2;
     static int number_three=3;
     static int number_four=4;
-    static int number_5=5;
-    static int number_6=6;
+    static int number_five=5;
+    static int number_six=6;
 
+    /**
+     *
+     * @param file to reaf the player's details
+     * @return the player's list
+     */
     public static ArrayList Read(File file) {
         try {
+            //reading a file using filereader
             FileReader file_reader = new FileReader(file);
             BufferedReader buffered_reader = new BufferedReader(file_reader);
             String line = buffered_reader.readLine();
-            String[] details;
+            String[] details;   // array to store player's details
             while (line != null) {
-                details = line.split(",");
-                String name = details[number_zero];
-                int age = Integer.parseInt(details[number_one]);
-                int weight = Integer.parseInt(details[number_two]);
-                String role = details[number_three];
+                details = line.split(",");    // split the values at the comma
+                String name = details[number_zero];   // storing name at starting index of an array
+                int age = Integer.parseInt(details[number_one]);  // storing player's age at first index
+                int weight = Integer.parseInt(details[number_two]);  //storing player's weight at second index
+                String role = details[number_three];  // store player's position
+                // check if player is a batsman
                 if (role.equalsIgnoreCase("BATSMAN")) {
-                    int runs = Integer.parseInt(details[number_four]);
-                    int batsman_experience = Integer.parseInt((details[number_5]));
-                    String style=details[number_6];
+                    int runs = Integer.parseInt(details[number_four]);  // store player's runs
+                    int batsman_experience = Integer.parseInt((details[number_five])); // store player's experience
+                    String style=details[number_six];  //store player's style
                     Batsman batsman = new Batsman(name, age, weight,runs,batsman_experience,style);
-                    player_list.add(batsman);}
+                    player_list.add(batsman); //adding batsman to the list
+                }
 
+                // if player is a bowler
                 else if (role.equalsIgnoreCase("BOWLER")) {
-                    int wickets = Integer.parseInt(details[number_four]);
-                    int bowler_experience = Integer.parseInt(details[number_5]);
-                    String style=details[number_6];
+                    int wickets = Integer.parseInt(details[number_four]); //store player's wickets
+                    int bowler_experience = Integer.parseInt(details[number_five]);  //store player's experience
+                    String style=details[number_six];  //store player's style
                     Bowler bowler = new Bowler(name, age, weight,wickets, bowler_experience,style);
-                    player_list.add(bowler);}
-                line = buffered_reader.readLine();
+                    player_list.add(bowler);   //adding bowler to the list
+                }
+                line = buffered_reader.readLine();  //reading the next line
             }
         }
         catch (IOException | NumberFormatException e) {
         }
-    return player_list;
+        return player_list;   //return player's list
     }}
-
